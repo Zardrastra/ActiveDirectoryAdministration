adbf0ac851e6bb195fc0ff5c5ecbc8af214e9b39
Zardrastra <johndundass@gmail.com>
2017-07-27 17:34:28 +0100

    Update MailboxDatabase-to-PST.ps1
    Corrected some logic issues which was causing a loop when the PST did not exist on the remote share and $ExportStatus was equal to $Null

--- MailboxDatabase-to-PST.ps1
+++ MailboxDatabase-to-PST.ps1
@@ -1,5 +1,7 @@
 #Create table of NT Account Aliases which match the rule "AccountDisabled and which are "UserMailbox"
+Write-host "Collecting Disabled Mailbox information for backup"
 $Mailboxes = Get-Mailbox -ResultSize Unlimited | where-object { $_.ExchangeUserAccountControl -like "AccountDisabled" -and $_.RecipientTypeDetails -eq 'UserMailbox' } | select-object alias
+clear-host
 $Global:RunningStatuses = "Completed",  "InProgress", "Failed", "Queued"
 #Variables controling backup folder directory
 # $ExcDomainControler is the Domain Controller
@@ -15,62 +17,74 @@ function ExchangeWaitLoop {
 $global:starttime = get-date
 clear-host
 
-    
-    [string] $Global:filepath = $Global:LocalShare + $pstname + ".pst"
-    $Global:InitialPST = Get-MailboxExportRequest -Mailbox $pstname | Get-MailboxExportRequestStatistics
+    [string] $Global:PSTName = $EmployeeMailboxName + ".pst"
+    [string] $Global:filepath = $Global:LocalShare + $EmployeeMailboxName + $PSTName
+    $Global:InitialPST = Get-MailboxExportRequest -Mailbox $EmployeeMailboxName | Get-MailboxExportRequestStatistics
     $Global:ExportStatus = $InitialPST.Status
-    $RunningStatuses = "Completed", "InProgress", "Failed", "Queued"
-    $NTAccount = Get-ADuser pstname
+    $NTAccount = Get-ADuser $EmployeeMailboxName
     $Global:EmployeeName = $NTAccount.Name
 
     if ($RunningStatuses -notcontains $ExportStatus){
+		$RemoteDir = Get-ChildItem $RemoteShare
+		$RemotePST = $RemoteDir.Name
+		if($RemotePST -notcontains $PSTName){
 @"
 Beginning PST Backup process for $EmployeeName
 
 ******************************************
 "@
-                New-MailboxExportRequest -DomainController $ExcDomainControler -mailbox $pstname -FilePath $filepath
+                New-MailboxExportRequest -DomainController $ExcDomainControler -mailbox $EmployeeMailboxName -FilePath $filepath
+                Start-Sleep -s 900
+				}
     }
     [Bool]$global:ProcessActive = $True
 
 
     while ($ProcessActive -eq $True){
-        $global:ProcessingQueue = Get-MailboxExportRequest -Mailbox $pstname | Get-MailboxExportRequestStatistics 
+        
+        $Global:ProcessingQueue = Get-MailboxExportRequest -Mailbox $EmployeeMailboxName | Get-MailboxExportRequestStatistics 
 
         #Find mailbox status after running backup request 
         $Global:PercentComplete = $ProcessingQueue.PercentComplete
-        $global:ExportStatus = $ProcessingQueue.Status
+        $Global:ExportStatus = $ProcessingQueue.Status
 
 		    if ($ExportStatus -eq "Completed"){
             #Test to check if the mailbox process is complete, ends loop by setting Process Active flag to false
 		    clear-host
-		    [Bool]$global:ProcessActive = $False
+		    [Bool]$Global:ProcessActive = $False
 			[Bool]$PSTExists = test-path $filepath
-			$ProcessingQueue = Get-MailboxExportRequest -Mailbox $pstname | Get-MailboxExportRequestStatistics 
+			$ProcessingQueue = Get-MailboxExportRequest -Mailbox $EmployeeMailboxName | Get-MailboxExportRequestStatistics 
 
 			#Find mailbox status after running backup request 
 			$PercentComplete = $ProcessingQueue.PercentComplete
 			$ExportStatus = $ProcessingQueue.Status
 				if($ExportStatus -eq "Completed"){
 					if ($PSTExists -eq $True){
-						"Mailbox copy for User $pstname completed, copying to remote share."
-                        Start-Sleep -s 15
+						"Mailbox copy for User $EmployeeMailboxName completed, copying to remote share."
+                        Start-Sleep -s 25
 						move-item $filepath $RemoteShare
 						#clear-host
 						}
 				}
 				Elseif($PSTExists -eq $False){
-				"No PST for user $pstname exists, skipping to next user"
+				"No PST for user $EmployeeMailboxName exists, skipping to next user"
 				}
 		    }
             Elseif($ExportStatus -eq "Failed"){
 
-            $PSTCopyErr = "PST Process For user $pstname failed"
+            $PSTCopyErr = "PST Process For user $EmployeeMailboxName failed"
             write-host $PSTCopyErr
             $loglocation = $LocalShare + "output.txt"
             $PSTCopyErr >> $loglocation
             "Press any key to continue"
             pause
+            }
+            Elseif($ExportStatus -eq $null){
+            [Bool]$Global:ProcessActive = $False
+@"
+Mailbox backup for $EmployeeName already completed
+Starting next mailbox copy
+"@
             }
 
 		    else {
@@ -97,12 +111,15 @@ clear-host
 "Refreshing backup progress..."
 		}
 	}
-    "Next mailbox backup in 5 seconds"
+@"
+    Mailbox for $EmployeeName backed up
+    Next mailbox backup in 5 seconds
+"@
     Start-Sleep -s 5
     clear-host
 }
 
 foreach ($mailbox in $Mailboxes) {
-$global:pstname = $mailbox.alias
+$global:EmployeeMailboxName = $mailbox.alias
 ExchangeWaitLoop
 }
